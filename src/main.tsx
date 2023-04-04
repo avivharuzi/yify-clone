6d296a802d76e53b067061b43836eab59639d64c
Aviv Haruzi <avivharuzi@gmail.com>
2023-04-04 22:01:36 +0300

    add material ui

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
